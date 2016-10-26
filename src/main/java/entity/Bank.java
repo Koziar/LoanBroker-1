3867dc8ac0aaee6823973d2ea2049c52accbc7ff
Joachim <joachim@Joachims-MacBook-Pro.local>
2016-10-26 10:41:01 +0200

    Changes in package name

--- src/main/java/entity/Bank.java
+++ src/main/java/entity/Bank.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package loanbroker.entity;
+package entity;
 
 /**
  *
  * @author Mathias
  */
-public class Message {
+public class Bank {
     
 }
