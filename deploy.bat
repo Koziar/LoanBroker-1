c468ebcc8f7c4074c5849a39f4601f8f18dc61ab
Mathias Pedersen <kontakt@mathiaspedersen.dk>
2016-12-02 13:41:42 +0100

    Changes made to run script

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,7 @@
+echo off
+start cmd /k mvn clean install
+echo Wait for Maven to finish building before proceeding
+pause
 start cmd /k mvn exec:java -Dexec.mainClass="loanbroker.Aggregator"
 start cmd /k mvn exec:java -Dexec.mainClass="loanbroker.GetBanks"
 start cmd /k mvn exec:java -Dexec.mainClass="loanbroker.GetCreditScore"
