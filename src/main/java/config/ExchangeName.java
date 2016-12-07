4a9978d5acfe87571c9a8a50acbd0f93d344952d
Mathias Pedersen <kontakt@mathiaspedersen.dk>
2016-12-07 09:59:47 +0100

    File with exchange name constants added

---
+++ src/main/java/config/ExchangeName.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+/**
+ *
+ * @author Mathias
+ */
+public class ExchangeName {
+
+    public static final String OUTPUT_LOAN_REQUEST = "output_loan_request";
+    public static final String OUTPUT_GET_BANKS = "output_get_banks";
+}
